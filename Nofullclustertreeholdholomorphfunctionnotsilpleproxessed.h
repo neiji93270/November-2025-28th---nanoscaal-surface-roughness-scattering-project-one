afe4aec20e814f4e27a33e86b165f9d4a53622ca
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-28 13:05:53 +0100

    Create Nofullclustertreeholdholomorphfunctionnotsilpleproxessed.h

---
+++ Nofullclustertreeholdholomorphfunctionnotsilpleproxessed.h
@@ -0,0 +1,4 @@
+//we need dimensionless processes here 
+//zo we will take schrodinger equation to compute
+//'egates hhpothesis of soft shadows. the bottleneck here
+//will be tonprebent 10 ciphers after comma errors 
